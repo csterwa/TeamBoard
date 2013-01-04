78f71f029f10fb6c41cc69a1898c28a2fc81a3c6
Chris Sterling <csterling@rallydev.com>
2013-01-03 16:53:34 -0800

    Got actual names of Teams from WSAPI Project to show as column headers

--- TeamBoardColumn.js
+++ TeamBoardColumn.js
@@ -5,9 +5,32 @@ Ext.define('Rally.app.teamboard.TeamBoardColumn', {
     alias:'widget.rallyteamcolumn',
 
     constructor:function (config) {
-        config = Ext.apply({
-            displayValue:config.teamRecord.get('projectRef')
-        }, config);
+        this.projectModel = Rally.data.ModelFactory.getModel({
+            type: 'Project',
+            success: function(model) {
+
+                var oid = Rally.util.Ref.getOidFromRef(config.teamRecord.get('projectRef'));
+
+                model.load(oid, {
+                    success: function(record) {
+                        debugger;
+                        config = Ext.apply({
+                            displayValue: record.get('Name')
+                        }, config);
+                        this.refresh(config);
+                    },
+                    failure: function() {
+                        //if the project doesn't exist or we don't have access to it.
+                        //let our owner handle the error situation.
+                        this.fireEvent('invalidvalue');
+                    },
+                    fetch: ['Name', 'Workspace'],
+                    scope: this
+                });
+
+            },
+            scope: this
+        });
 
         this.callParent(arguments);
     },
